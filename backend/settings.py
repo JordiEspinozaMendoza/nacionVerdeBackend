0d799060c5e02214a97db3d0c6ede03d90285663
JordiEspinozaMendoza <jordi8101@gmail.com>
2022-01-11 15:33:03 -0800

    Added getcwd app config

--- backend/settings.py
+++ backend/settings.py
@@ -168,3 +168,12 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+if os.getcwd() == "/app":
+    import dj_database_url
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES["default"].update(db_from_env)
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
